077515aac0663c04dd4a221c55d71a53db7eedea
rtrebeccarasody-ai <rtrebeccarasody@gmail.com>
2026-05-12 12:10:26 +0300

    Delete api directory

--- api/forex.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  const key = process.env.TWELVEDATA_KEY;
-  const { symbol } = req.query;
-  const url = `https://api.twelvedata.com/quote?symbol=${symbol}&apikey=${key}`;
-  const r = await fetch(url);
-  const data = await r.json();
-  res.status(200).json(data);
-}
